824adc3a7e72c791487dc882a8ad4896282cd925
YEWOONY <dge04206@gmail.com>
2019-03-25 17:25:48 +0900

    Update new block

--- miniblock/ts_blockchain.py
+++ miniblock/ts_blockchain.py
@@ -5,7 +5,28 @@ class Blockchain(object):
 
     def new_block(self):
         # create new block, add it to chain
-        pass
+        """
+          블록체인에 새로운 블록 만들기
+
+          :param proof: <int> proof 는 Proof of Work 알고리즘에 의해서 제공된다.
+          :param previous_hash: (Optional) <str> 이전 블록의 해쉬값
+          :return : <dict> 새로운 블록
+
+          """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # 거래 내역 초기화
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self):
         # add new transaction to list of transaction
