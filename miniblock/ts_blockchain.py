136b3040d7440332923df533d1057332820430a0
seohyoj55 <seohyoj55@naver.com>
2019-04-04 17:02:36 +0900

    Update end block

--- miniblock/ts_blockchain.py
+++ miniblock/ts_blockchain.py
@@ -4,7 +4,7 @@ from textwrap import dedent
 from time import time
 from uuid import uuid4
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 class Blockchain(object):
@@ -95,30 +95,80 @@ class Blockchain(object):
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
 
+
 # Instantiate our Node
 app = Flask(__name__)
 
 # generate a gloablly unique address for this node
-node_identifier = str(uuid4()).replace('-','')
+node_identifier = str(uuid4()).replace('-', '')
 
 # Instantiate the Blockchain
 blockchain = Blockchain()
+
 
 @app.route('/mine', methods=['GET'])
 def mine():
     return "We'll mine a new Block"
 
+
 @app.route('/transactions/new', methods=['POST'])
 def new_transactions():
     return "We'll add a new transaction"
 
+
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
-        'chain':blockchain.chain,
-        'length':len(blockchain.chain),
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
     }
     return jsonify(response),200
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port= 5000)
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # 요청된 필드가 POST 된 데이터인지 확인하는
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # 새로운 거래 생성
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # 다음 블록의 proof 값을 얻어내기 위해 POW 알고리즘을 수행한다.
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # proof 값을 찾으면(채굴에 성공하면) 보상을 준다.
+    # sender의 주소를 0으로 한다. (원래 거래는 송신자, 수신자가 있어야 하는데 챌굴에 대한 보상으로 얻은 코인은 sender 가 없다.)
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    #
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
