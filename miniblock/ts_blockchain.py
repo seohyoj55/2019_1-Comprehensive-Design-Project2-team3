27dd6d9a584889040cf608a7239267f706665b9d
YEWOONY <dge04206@gmail.com>
2019-04-04 16:21:07 +0900

    Update POW Algorithm

--- miniblock/ts_blockchain.py
+++ miniblock/ts_blockchain.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
 
 class Blockchain(object):
     def _init_(self):
@@ -61,4 +62,32 @@ class Blockchain(object):
         # hashes a block
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+
+    def proof_of_work(self, last_proof):
+        """
+         POW 알고리즘
+         - 앞에서 0의 개수가 4개가 나오는 hash(pp')을 만족시키는 p'를 찾음
+         - p : 이전 블록의 proof, p': 새로운 블록의 proof
+
+         :param last_proof : <int>
+         :return : <int>
+         """
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+            Proof 검증 방법 : hash(last_proof, proof)의 값의 앞의 4자리가 0인가?
+
+            :param last_proof : <int> 이전 블록의 proof 값
+            :param proof : <int> 현재 블록의 proof 값
+            :return : <bool> 옳다면 true 값 그렇지 않으면 false 값 반환
+            """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
 
