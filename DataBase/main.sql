f4d12332ffecf695c413129285b612b6303cff93
Hyojoo Seo <31160822+seohyoj55@users.noreply.github.com>
2019-05-26 15:42:02 +0900

    Add Patient Table

--- DataBase/main.sql
+++ DataBase/main.sql
@@ -1,1 +1,11 @@
-#
+CREATE TABLE Patient( /*환자 초기 정보*/
+PID int(5) NOT NULL,
+name char(4) NOT NULL,
+telecom int(11),
+address varchar(50);
+gender enum('F','M') NOT NULL,
+birthDate int(8) NOT NULL,
+generalPractitioner char(4),
+bloodtype enum('A','B','O','AB'),
+PRIMARY KEY(PID)
+);
