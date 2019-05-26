8f606930bd8c20057e01859487b5e5008151ee37
Hyojoo Seo <31160822+seohyoj55@users.noreply.github.com>
2019-05-26 16:02:48 +0900

    Add comment

--- DataBase/main.sql
+++ DataBase/main.sql
@@ -1,38 +1,38 @@
 #Patient Table
-CREATE TABLE Patient( /*환자 초기 정보*/
-PID int(5) NOT NULL, /*환자 아이디*/
-name char(4) NOT NULL, /*환자 이름*/
-telecom int(11), /*환자 전화번호*/
-address varchar(50); /*환자 주소*/
-gender enum('F','M') NOT NULL, /*환자 성별*/
-birthDate int(8) NOT NULL, /*환자 생년월일*/
-generalPractitioner char(4), /*배정된 주치의*/
-bloodtype enum('A','B','O','AB'), /*환자 혈액형*/
-PRIMARY KEY(PID)
-);
-
-#Doctor Table
-CREATE TABLE Doctor( /*의사 초기 정보*/
-DID int(5) NOT NULL, /*의사 아이디*/
-name char(4) NOT NULL, /*의사 이름*/
-telecom int(11), /*의사 전화번호*/
-address varchar(50); /*의사 주소*/
-gender enum('F','M') NOT NULL, /*의사 성별*/
-birthDate int(8) NOT NULL, /*의사 생년월일*/
-qualification int(5) NOT NULL, /*의사 면허번호*/
-department varchar(30), /*의사 진료과목*/
-PRIMARY KEY(DID)
-);
-
-#Nurse Table
-CREATE TABLE Nurse( /*간호사 초기 정보*/
-NID int(5) NOT NULL,
-name char(4) NOT NULL,
-telecom int(11),
-address varchar(50),
-gender enum('F','M') NOT NULL,
-birthDate int(8) NOT NULL,
-qualification int(5) NOT NULL,
+CREATE TABLE Patient(                             /*환자 초기 정보*/
+PID int(5) NOT NULL,                              /*환자 아이디*/
+name char(4) NOT NULL,                            /*환자 이름*/
+telecom int(11),                                  /*환자 전화번호*/
+address varchar(50);                              /*환자 주소*/
+gender enum('F','M') NOT NULL,                    /*환자 성별*/
+birthDate int(8) NOT NULL,                        /*환자 생년월일*/
+generalPractitioner char(4),                      /*배정된 주치의*/
+bloodtype enum('A','B','O','AB'),                 /*환자 혈액형*/
+PRIMARY KEY(PID)              
+);              
+              
+#Doctor Table             
+CREATE TABLE Doctor(                              /*의사 초기 정보*/
+DID int(5) NOT NULL,                              /*의사 아이디*/
+name char(4) NOT NULL,                            /*의사 이름*/
+telecom int(11),                                  /*의사 전화번호*/
+address varchar(50);                              /*의사 주소*/
+gender enum('F','M') NOT NULL,                    /*의사 성별*/
+birthDate int(8) NOT NULL,                        /*의사 생년월일*/
+qualification int(5) NOT NULL,                    /*의사 면허번호*/
+department varchar(30),                           /*의사 진료과목*/
+PRIMARY KEY(DID)              
+);              
+              
+#Nurse Table              
+CREATE TABLE Nurse(                               /*간호사 초기 정보*/
+NID int(5) NOT NULL,                              /*간호사 아이디*/
+name char(4) NOT NULL,                            /*간호사 이름*/
+telecom int(11),                                  /*간호사 전화번호*/
+address varchar(50),                              /*간호사 주소*/
+gender enum('F','M') NOT NULL,                    /*간호사 성별*/
+birthDate int(8) NOT NULL,                        /*간호사 생년월일*/
+qualification int(5) NOT NULL,                    /*간호사 면허번호*/
 PRIMARY KEY(NID)
 );
 
