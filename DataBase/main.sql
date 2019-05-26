d0f23b0e423964c2b6bd02cb61947384fc475ab3
YEWOONY <39361367+YEWOONY@users.noreply.github.com>
2019-05-26 15:43:55 +0900

    Update main.sql
    Add Doctor Table

--- DataBase/main.sql
+++ DataBase/main.sql
@@ -9,3 +9,15 @@ generalPractitioner char(4),
 bloodtype enum('A','B','O','AB'),
 PRIMARY KEY(PID)
 );
+
+CREATE TABLE Doctor( /*의사 초기 정보*/
+DID int(5) NOT NULL,
+name char(4) NOT NULL,
+telecom int(11),
+address varchar(50);
+gender enum('F','M') NOT NULL,
+birthDate int(8) NOT NULL,
+qualification int(5) NOT NULL,
+department varchar(30),
+PRIMARY KEY(DID)
+);
