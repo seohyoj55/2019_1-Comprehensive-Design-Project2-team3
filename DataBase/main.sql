7bd2fc50a4dfe6a9d0be376617fb2f70ce2b9af8
YEWOONY <39361367+YEWOONY@users.noreply.github.com>
2019-05-26 17:00:17 +0900

    Update main.sql
    Update Examination Table

--- DataBase/main.sql
+++ DataBase/main.sql
@@ -38,22 +38,14 @@ PRIMARY KEY(NID)
 
 #Examination Table
 CREATE TABLE Examination(                         /*문진 목록*/
-EXID int(5), NOT NULL,                            /*문진 번호*/
-PID int(5), NOT NULL,                             /*환자 아이디*/
+ExNo int(5) NOT NULL,                             /*문진 번호*/
+PID int(5) NOT NULL,                              /*환자 아이디*/
 DID int(5) NOT NULL,                              /*의사 아이디*/
 Edate DATE NOT NULL,                              /*일시*/
-writer char(4),                                   /*작성자*/
 imp varchar(20),                                  /*진단명*/
-BP int,                                           /*혈압*/
-RR int,                                           /*호흡수*/
-BT int,                                           /*체온*/
-PR int,                                           /*맥박*/
-weight int,                                       /*체중*/
-height int,                                       /*신장*/
 Disability boolean,                               /*장애유무*/
 Drug boolean,                                     /*투약 or 복용약물 유무*/
 family boolean,                                   /*가족병력 유무*/
-LOC enum('AL','DR','ST','SE','CO'),               /*의식상태-명료(AL), 기면(DR), 혼미(ST), 반혼수(SE), 혼수(CO)*/
 PH varchar(50),                                   /*과거력*/
 PRIMARY KEY(EXID),
 FOREIGN KEY(PID) REFERENCES Patient(PID),
