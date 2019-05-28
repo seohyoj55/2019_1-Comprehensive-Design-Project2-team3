06b70eeb353dd852d924eba9eaa06d8a97a4c572
YEWOONY <39361367+YEWOONY@users.noreply.github.com>
2019-05-28 23:53:52 +0900

    Add nurse.php

---
+++ PHP/nurse.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>간호사 메인</title>
+  </head>
+  <body>
+    <h1><a href="index.php">메인으로 돌아가기</a></h1>
+    <ol>
+      <li><a href="select.php">조회</a>
+      <li><a href="ninsert.php">입력</a>
+    </ol>
+  </body>
+</html>
