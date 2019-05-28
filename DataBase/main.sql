75bcb714b74d18f09a559a663668745f9e89f934
Hyojoo Seo <31160822+seohyoj55@users.noreply.github.com>
2019-05-28 21:19:12 +0900

    Add DROP part

--- DataBase/main.sql
+++ DataBase/main.sql
@@ -1,3 +1,16 @@
+SET @tables = NULL;
+SELECT
+    GROUP_CONCAT(table_schema, '.', table_name)
+INTO @tables FROM
+    information_schema.tables
+WHERE
+    table_schema = 'emr_db'; -- specify DB name here.
+
+SET @tables = CONCAT('DROP TABLE ', @tables);
+PREPARE stmt FROM @tables;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
 #Patient Table
 CREATE TABLE Patient(                             /*환자 초기 정보*/
 PID int(5) NOT NULL,                              /*환자 아이디*/
