8a3041dcca51232b3908f2f44d5b87edbbfbd42b
YEWOONY <39361367+YEWOONY@users.noreply.github.com>
2019-05-28 23:53:08 +0900

    Add index.php

---
+++ PHP/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>메인</title>
+  </head>
+  <body>
+    <ol>
+      <li><a href="doctor.php">의사 메인</a>
+      <li><a href="nurse.php">간호사 메인</a>
+    </ol>
+  </body>
+</html>
