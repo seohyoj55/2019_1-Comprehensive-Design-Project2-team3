e0b2413940c7d03546d9c9e6ec00ac5197df1131
Hyojoo Seo <31160822+seohyoj55@users.noreply.github.com>
2019-05-30 16:03:28 +0900

    Update main.sql

--- DataBase/main.sql
+++ DataBase/main.sql
@@ -11,65 +11,71 @@ PREPARE stmt FROM @tables;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
-#Patient Table
-CREATE TABLE Patient(                             /*환자 초기 정보*/
-PID int(5) NOT NULL,                              /*환자 아이디*/
-name char(4) NOT NULL,                            /*환자 이름*/
-telecom int(11),                                  /*환자 전화번호*/
-address varchar(50);                              /*환자 주소*/
-gender enum('F','M') NOT NULL,                    /*환자 성별*/
-birthDate int(8) NOT NULL,                        /*환자 생년월일*/
-generalPractitioner char(4),                      /*배정된 주치의*/
-bloodtype enum('A','B','O','AB'),                 /*환자 혈액형*/
-PRIMARY KEY(PID)              
-);              
-              
-#Doctor Table             
-CREATE TABLE Doctor(                              /*의사 초기 정보*/
-DID int(5) NOT NULL,                              /*의사 아이디*/
-name char(4) NOT NULL,                            /*의사 이름*/
-telecom int(11),                                  /*의사 전화번호*/
-address varchar(50);                              /*의사 주소*/
-gender enum('F','M') NOT NULL,                    /*의사 성별*/
-birthDate int(8) NOT NULL,                        /*의사 생년월일*/
-qualification int(5) NOT NULL,                    /*의사 면허번호*/
-department varchar(30),                           /*의사 진료과목*/
-PRIMARY KEY(DID)              
-);              
-              
-#Nurse Table              
-CREATE TABLE Nurse(                               /*간호사 초기 정보*/
-NID int(5) NOT NULL,                              /*간호사 아이디*/
-name char(4) NOT NULL,                            /*간호사 이름*/
-telecom int(11),                                  /*간호사 전화번호*/
-address varchar(50),                              /*간호사 주소*/
-gender enum('F','M') NOT NULL,                    /*간호사 성별*/
-birthDate int(8) NOT NULL,                        /*간호사 생년월일*/
-qualification int(5) NOT NULL,                    /*간호사 면허번호*/
-PRIMARY KEY(NID)
+CREATE TABLE Doctor (
+    DID INT(5) NOT NULL AUTO_INCREMENT,
+    name CHAR(4) NOT NULL,
+    telecom CHAR(13),
+    address VARCHAR(50),
+    gender ENUM('F', 'M') NOT NULL,
+    birthDate DATE NOT NULL,
+    qualification INT(5) NOT NULL,
+    department VARCHAR(30),
+    PW VARCHAR(15),
+    PRIMARY KEY (DID)
 );
 
-#Examination Table
-CREATE TABLE Examination(                         /*문진 목록*/
-ExNo int(5) NOT NULL,                             /*문진 번호*/
-PID int(5) NOT NULL,                              /*환자 아이디*/
-DID int(5) NOT NULL,                              /*의사 아이디*/
-Edate DATE NOT NULL,                              /*일시*/
-imp varchar(20),                                  /*진단명*/
-Disability boolean,                               /*장애유무*/
-Drug boolean,                                     /*투약 or 복용약물 유무*/
-family boolean,                                   /*가족병력 유무*/
-PH varchar(50),                                   /*과거력*/
-PRIMARY KEY(EXID),
-FOREIGN KEY(PID) REFERENCES Patient(PID),
-FOREIGN KEY(DID) REFERENCES Patient(DID)
+CREATE TABLE Nurse (
+    NID INT(5) NOT NULL AUTO_INCREMENT,
+    name CHAR(4) NOT NULL,
+    telecom CHAR(13),
+    address VARCHAR(50),
+    gender ENUM('F', 'M') NOT NULL,
+    birthDate DATE NOT NULL,
+    qualification INT(5) NOT NULL,
+    department VARCHAR(30),
+    PW VARCHAR(15),
+    PRIMARY KEY (NID)
 );
 
-#Doctor's Input Table
+CREATE TABLE Patient (
+    PID INT(5) NOT NULL AUTO_INCREMENT,
+    name CHAR(4) NOT NULL,
+    telecom CHAR(13),
+    address VARCHAR(50),
+    gender ENUM('F', 'M') NOT NULL,
+    birthDate DATE NOT NULL,
+    generalPractitioner INT(5),
+    bloodtype ENUM('A', 'B', 'O', 'AB'),
+    PRIMARY KEY (PID),
+    FOREIGN KEY (generalPractitioner)
+        REFERENCES Doctor (DID)
+);
+
+CREATE TABLE Examination (
+    ExNo INT(5) NOT NULL AUTO_INCREMENT,
+    PID INT(5) NOT NULL,
+    DID INT(5) NOT NULL,
+    Edate TIMESTAMP DEFAULT NOW(),
+    imp VARCHAR(20),
+    weight INT,
+    height INT,
+    Disability BOOLEAN,
+    Drug BOOLEAN,
+    family BOOLEAN,
+    PH VARCHAR(50),
+    opinion VARCHAR(500),
+    PRIMARY KEY (ExNo),
+    FOREIGN KEY (PID)
+        REFERENCES Patient (PID),
+    FOREIGN KEY (DID)
+        REFERENCES Doctor (DID)
+);
+
 CREATE TABLE DocInput (
-    DINo INT(5) NOT NULL,
-    PID CHAR(5) NOT NULL,
-    DID CHAR(5) NOT NULL,
+    DINo INT(5) NOT NULL AUTO_INCREMENT,
+    PID INT(5) NOT NULL,
+    DID INT(5) NOT NULL,
+    Ddate TIMESTAMP DEFAULT NOW(),
     opinion VARCHAR(500),
     prescription VARCHAR(500),
     PRIMARY KEY (DINo),
@@ -79,11 +85,11 @@ CREATE TABLE DocInput (
         REFERENCES Doctor (DID)
 );
 
-#Nurse's Input Table
 CREATE TABLE NurInput (
-   NINo INT(5) NOT NULL,
-    PID CHAR(5) NOT NULL,
-    NID CHAR(5) NOT NULL,
+    NINo INT(5) NOT NULL AUTO_INCREMENT,
+    PID INT(5) NOT NULL,
+    NID INT(5) NOT NULL,
+    Ndate TIMESTAMP DEFAULT NOW(),
     BP INT,
     RR INT,
     BT DECIMAL(3 , 1 ),
